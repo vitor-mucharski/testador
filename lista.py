0833bc9719d013e40dbb89f6fadbb1539ca17b78
vitor-mucharski <vitorchoni@gmail.com>
2026-01-06 13:44:19 -0300

    arquivo python

---
+++ lista.py
@@ -0,0 +1,8 @@
+list = ["lss", "javascript", "jp"]
+valor = input("quer saber linguagens? s/n: ").lower()
+if valor == "s":
+    print(list)
+elif valor == "n":
+    print("tchau")
+else:
+    print("erro")
